b8efcf26393bc5eef54657177c8b574e128b251a
Dry Fish <realmg51@gmail.com>
2026-06-29 06:23:37 +0700

    Create Variables.cs to demonstrate variable usage in C#
    Added a simple C# program demonstrating variable usage.

---
+++ src/Variables/Variables.cs
@@ -0,0 +1,17 @@
+// this program tells you how to use variables in C#
+using System;
+// this program is simple, too, so no need namespace
+class VariableUse // you need to make your class easy to read, it helps everyone can read understand your code and helps you so much in furture
+{
+    static void Main() // Every C# or other languages program need entry point like Main() or main()
+    {
+        // int contains numbers:
+        int number = 23; // you can write any number in here.
+        float pi = 3.14f; // you need to write "F" or "f" after values (before ';')
+        // because i not usually use float, double so i don't know more, sorry:(
+        // use it:
+        Console.WriteLine("my favourite number is: " + number);
+        Console.WriteLine("Three first number of pi is: " + pi);
+    }
+// this code is example, not docs, so it may contains some wrong or deprecated thing, so you can find official docs by Microsoft on internet
+        

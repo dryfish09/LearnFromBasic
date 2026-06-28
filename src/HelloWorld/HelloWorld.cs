04ca19a896ed9f41c05726364965798a1d598dad
Dry Fish <realmg51@gmail.com>
2026-06-28 20:55:22 +0700

    Add HelloWorld program that prints a greeting

---
+++ src/HelloWorld/HelloWorld.cs
@@ -0,0 +1,10 @@
+using System;
+// for this code, just prints hello, no need namespace:)
+class Program
+{
+    // entry point should in class
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    } // method
+} // class
